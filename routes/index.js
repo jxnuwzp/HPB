c2d8434b1ea99b32a76a2ab41cc517a1ad81562a
mingiswell <mingiswell@hotmail.com>
2016-11-01 23:52:33 +0800

    add the webpages individually

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,60 @@ module.exports = function(app) {
     res.render('index', { title: 'haopingbang' });
   });
 
-  app.get('/login', function (req, res) {
-    res.render('login');
-  });
-
-  app.get('/register', function (req, res) {
+  app.get('/shopregister', function (req, res) {
     res.render('register');
   });
 
-  app.get('/shop', function (req, res) {
-    res.render('home');
+  app.get('/reviewerregister', function (req, res) {
+    res.render('register');
   });
 
+  app.get('/shoplogin', function (req, res) {
+    res.render('login');
+  });
+
+  app.get('/reviewerlogin', function (req, res) {
+    res.render('login');
+  });
+
+  app.get('/shop', function (req, res) {
+    res.render('shop');
+  });
+
+  app.get('/shopmanage', function (req, res) {
+    res.render('shopmanage');
+  });
+
+  app.get('/shoptask', function (req, res) {
+    res.render('shoptask');
+  });
+
+  app.get('/shoppublishtask', function (req, res) {
+    res.render('shoppublishtask');
+  });
+
+  app.get('/shopcharge', function (req, res) {
+    res.render('shopcharge');
+  });
+ 
+
   app.get('/reviewer', function (req, res) {
-    res.render('home');
-  }); 
+    res.render('reviewer');
+  });
+
+  app.get('/reviewermanage', function (req, res) {
+    res.render('reviewermanage');
+  });
+
+  app.get('/reviewermoney', function (req, res) {
+    res.render('reviewermoney');
+  });
+
+  app.get('/reviewertask', function (req, res) {
+    res.render('reviewertask');
+  });
+
+  app.get('/task', function (req, res) {
+    res.render('task');
+  });
 };
